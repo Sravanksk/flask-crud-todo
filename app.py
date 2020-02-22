f985db18fc6a27e230df9ea44e15ae69c3cabae0
Sravan Kumar <sravan.kumar@fyle.in>
2020-02-22 17:07:52 +0530

    pass db as environment variable

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
+
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 
@@ -61,4 +63,4 @@ def update(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
