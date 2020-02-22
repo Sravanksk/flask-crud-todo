4ab8cfab903d5bb223cb675b9a7d6b359bb2fbd2
Sravan Kumar <sravan.kumar@fyle.in>
2020-02-22 17:26:36 +0530

    Revert "pass db as environment variable"
    This reverts commit f985db18fc6a27e230df9ea44e15ae69c3cabae0.

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import os
-
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
@@ -63,4 +61,4 @@ def update(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
